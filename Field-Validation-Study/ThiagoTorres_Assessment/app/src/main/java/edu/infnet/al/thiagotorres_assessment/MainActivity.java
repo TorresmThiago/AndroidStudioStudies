7f106ccc7f94fa37a4f1797bf98002e15b5fc113
Torres <torresmthiago@gmail.com>
2018-12-11 22:33:31 -0200

    Criação do layout da aplicação

--- Field-Validation-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorres_assessment/MainActivity.java
+++ Field-Validation-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorres_assessment/MainActivity.java
@@ -2,8 +2,27 @@ package edu.infnet.al.thiagotorres_assessment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 
 public class MainActivity extends AppCompatActivity {
+
+    @NotEmpty
+    EditText nome;
+
+    @NotEmpty
+    EditText loginEmail;
+
+    @NotEmpty
+    EditText senha;
+
+    @NotEmpty
+    EditText confirmarSenha;
+
+    @NotEmpty
+    EditText cpf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
