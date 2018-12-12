40f4c9bc6a30cfe8422fb39a7520a92daee1ca4b
Torres <torresmthiago@gmail.com>
2018-12-12 00:45:26 -0200

    Adição de anúncios e criação do fluxo do app

--- Field-Validation-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorres_assessment/MainActivity.java
+++ Field-Validation-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorres_assessment/MainActivity.java
@@ -1,29 +1,32 @@
 package edu.infnet.al.thiagotorres_assessment;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.InterstitialAd;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
-import com.mobsandgeeks.saripaar.annotation.Digits;
 import com.mobsandgeeks.saripaar.annotation.Email;
-import com.mobsandgeeks.saripaar.annotation.Max;
-import com.mobsandgeeks.saripaar.annotation.Min;
-import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
 import com.mobsandgeeks.saripaar.annotation.Pattern;
 
 import java.io.FileOutputStream;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity implements Validator.ValidationListener {
 
+    private InterstitialAd mInterstitialAd;
     Validator validator;
 
     @Pattern(regex = "^[a-zA-Z0-9]{4,10}$", message = "Nome inválido. Min. 6, Max. 10 char.")
@@ -48,6 +51,12 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
         nome = findViewById(R.id.nomeEditText);
         loginEmail = findViewById(R.id.loginEditText);
         senha = findViewById(R.id.senhaEditText);
@@ -64,6 +73,38 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
 
         validator = new Validator(this);
         validator.setValidationListener(this);
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                nome.setText("");
+                loginEmail.setText("");
+                senha.setText("");
+                confirmarSenha.setText("");
+                cpf.setText("");
+            }
+        });
 
     }
 
@@ -116,12 +157,14 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
             fos.flush();
             fos.close();
 
-            Toast.makeText(getApplicationContext(), "Contato salvo!", Toast.LENGTH_SHORT).show();
-
+            if (mInterstitialAd.isLoaded()) {
+                mInterstitialAd.show();
+            } else {
+                Toast.makeText(this, "O anúncio ainda não foi carregado!", Toast.LENGTH_LONG).show();
+            }
         }
         catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Erro ao salvar contato!", Toast.LENGTH_SHORT).show();
         }
-
     }
 }
