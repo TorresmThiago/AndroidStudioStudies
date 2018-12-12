04974f7771c79a4addd0382e4b6641a1a61e463b
Torres <torresmthiago@gmail.com>
2018-12-11 22:55:05 -0200

    Aplicação do Saripaar para verificação de campos preenchidos

--- Field-Validation-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorres_assessment/MainActivity.java
+++ Field-Validation-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorres_assessment/MainActivity.java
@@ -2,11 +2,20 @@ package edu.infnet.al.thiagotorres_assessment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Validator.ValidationListener {
+
+    Validator validator;
 
     @NotEmpty
     EditText nome;
@@ -23,10 +32,49 @@ public class MainActivity extends AppCompatActivity {
     @NotEmpty
     EditText cpf;
 
+    Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = findViewById(R.id.nomeEditText);
+        loginEmail = findViewById(R.id.loginEditText);
+        senha = findViewById(R.id.senhaEditText);
+        confirmarSenha = findViewById(R.id.confirmarSenhaEditText);
+        cpf = findViewById(R.id.cpfEditText);
+        cadastrar = findViewById(R.id.cadastrarUsuarioButton);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validator.validate();
+            }
+        });
+
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Toast.makeText(this, "Usuário cadastrado!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
