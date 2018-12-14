9fb4de0f8c0dc5c10e57ff1b4940ace6643efefe
Torres <torresmthiago@gmail.com>
2018-12-14 18:08:58 -0200

    Correção login Facebook

--- Login-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorresassessment/MainActivity.java
+++ Login-Study/ThiagoTorres_Assessment/app/src/main/java/edu/infnet/al/thiagotorresassessment/MainActivity.java
@@ -2,50 +2,79 @@ package edu.infnet.al.thiagotorresassessment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
+    private FirebaseAuth mAuth;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        callbackManager = CallbackManager.Factory.create();
+        loginButton = findViewById(R.id.loginWithFacebook);
+        mAuth = FirebaseAuth.getInstance();
 
-        LoginManager.getInstance().registerCallback(callbackManager,
-            new FacebookCallback<LoginResult>() {
-                @Override
-                public void onSuccess(LoginResult loginResult) {
-                    goToLoggedActivity();
-                }
-
-                @Override
-                public void onCancel() {
-                    Log.d("BBBBBBBBBBBBBBBB", String.valueOf(345));
-                }
-
-                @Override
-                public void onError(FacebookException exception) {
-                    Log.d("CCCCCCCCCCCC", String.valueOf(exception));
-                }
-            });
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void loginWithFacebook(View view) {
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        handleFacebookLogin(loginResult.getAccessToken());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                    }
+                });
+    }
+
+    public void handleFacebookLogin(AccessToken accessToken) {
+        AuthCredential authCredential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToLoggedActivity();
+                        }
+                    }
+                });
     }
 
     public void goToLoginActivity(View view) {
